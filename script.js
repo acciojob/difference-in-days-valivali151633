996ce587ea3de1a279befcca941f4fe6741ac9e3
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-17 19:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const parsedDate1 = new Date(date1);
+    const parsedDate2 = new Date(date2);
+
+    // Calculate the difference in milliseconds
+    const timeDiff = parsedDate2.getTime() - parsedDate1.getTime();
+
+    // Calculate the difference in days
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    return daysDiff;
 };
 
 // Do not change the code below.
